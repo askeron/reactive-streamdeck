58f69d15885dc8d842c275b3167571ba9f9e5543
Bjoern Kuhlen <bjoern@kuhlen.org>
2020-03-29 03:19:05 +0200

    added image labels and refactored

--- icon-renderer.js
+++ icon-renderer.js
@@ -17,36 +17,77 @@ async function getIconBuffer(icon, iconsize) {
 
 async function getIconSharp(icon, iconsize) {
 	if (icon.type === 'text') {
-		const pngBuffer = await sharp(text2png(icon.text, {
-			color: 'white',
-			backgroundColor: 'black',
-			padding: 14,
-			lineSpacing: 6,
-			textAlign: 'center'
-		}))
-		.resize(iconsize, iconsize, {fit: "contain"})
-		.png()
-		.toBuffer();
-	
-		// For some reason, adding an overlayWith command forces the final image to have
-		// an alpha channel, even if we call .flatten().
-		// To work around this, we have to overlay the image, render it as a PNG,
-		// then put that PNG back into Sharp, flatten it, and render raw.
-		// Seems like a bug in Sharp that we should make a test case for and report.
-		return sharp(pngBuffer)
+		return await getTextSharp(icon, iconsize)
 	} if (icon.type === 'image') {
-		const filePath = path.resolve(process.cwd(), icon.relativePath)
-		if (! await fileExists(filePath)) {
-			throw new Error("iconimage not found: "+filePath)
-		}
-		return sharp(filePath)
+		return await getImageSharp(icon, iconsize)
 	} if (icon.type === 'square') {
-		return getSquareSharp(icon.color.r, icon.color.g, icon.color.b, iconsize)
+		return await getSquareSharp(icon.color.r, icon.color.g, icon.color.b, iconsize)
 	} if (icon.type === 'blank') {
-		return getSquareSharp(0, 0, 0, iconsize)
+		return await getSquareSharp(0, 0, 0, iconsize)
 	} else {
 		throw new Error("illegal icon type: "+icon.type)
 	}
+}
+
+async function getTextSharp(icon, iconsize) {
+	return await correctAlphaChannel(sharp(text2png(icon.text, {
+		color: 'white',
+		//backgroundColor: 'black',
+		font: '20px sans-serif',
+		padding: 14,
+		lineSpacing: 6,
+		textAlign: 'center'
+	}))
+	.resize(iconsize, iconsize, {fit: "contain"})
+	)
+}
+
+async function correctAlphaChannel(sharpInstance) {
+	// For some reason, adding an overlayWith command forces the final image to have
+	// an alpha channel, even if we call .flatten().
+	// To work around this, we have to overlay the image, render it as a PNG,
+	// then put that PNG back into Sharp, flatten it, and render raw.
+	// Seems like a bug in Sharp that we should make a test case for and report.
+	return sharp(await (sharpInstance.png().toBuffer()))
+}
+
+async function getImageSharp(icon, iconsize) {
+	const filePath = path.resolve(process.cwd(), icon.relativePath)
+	if (! await fileExists(filePath)) {
+		throw new Error("iconimage not found: "+filePath)
+	}
+	let result = sharp(filePath)
+	if ('labels' in icon) {
+		result = result
+			.resize(iconsize, iconsize, {fit: "contain"})
+			.composite(await Promise.all(icon.labels.map(async (label) => {
+				const top = Math.round(label.position.top * iconsize)
+				const left = Math.round(label.position.left * iconsize)
+				const bottom = Math.round(label.position.bottom * iconsize)
+				const right = Math.round(label.position.right * iconsize)
+				const width = right - left
+				const height = bottom - top
+				const fullTransparentColor = {r:0,g:0,b:0,alpha:0}
+				const textPngBuffer = await(sharp(text2png(label.text, {
+						color: 'white',
+						//backgroundColor: 'black',
+						font: '30px sans-serif',
+						padding: 0,
+						lineSpacing: 6,
+						textAlign: 'center'
+					}))
+					.resize(width, height, {
+						fit: "contain",
+						background: fullTransparentColor,
+					})
+					.png()
+					.toBuffer()
+				)
+				return { input: textPngBuffer, top, left }
+			})))
+		result = await correctAlphaChannel(result)
+	}
+	return result
 }
 
 function getSquareSharp(r,g,b, iconsize){
