0d363cb6178e209386fac454b2a53c03e3082cf2
Bjoern Kuhlen <bjoern@kuhlen.org>
2021-12-16 23:45:54 +0100

    readded webview without websockets under legacy path

--- webview.js
+++ webview.js
@@ -65,7 +65,54 @@ module.exports = ({expressApp, getKeyRowCount, getKeyColumnCount, simulateKeyDow
         </html>
         `)
     });
-    
+
+    expressApp.get('/legacy', function(req, res, next){
+        const rows = getKeyRowCount()
+        const columns = getKeyColumnCount()
+        const keyCount = rows * columns
+        let iconHtml = ""
+        for (let row = 0; row < rows; row++) {
+            iconHtml += "<p>\n"
+            for (let column = 0; column < columns; column++) {
+                const i = row * columns + column
+                iconHtml += `<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2NgYGD4DwABBAEAcCBlCwAAAABJRU5ErkJggg==" id="image${i}" onclick="simulateClick(${i})">\n`
+            }
+            iconHtml += "</p>\n"
+        }
+        res.send(`
+        <html>
+            <head>
+                <style>
+                    p {
+                        text-align: center;
+                    }
+                    img {
+                        padding: 3px 6px;
+                    }
+                </style>
+            </head>
+            <body style="background-color: black;">
+            ${iconHtml}
+        
+                <script>
+                    function updateImages() {
+                        for (let i = 0; i < ${keyCount}; i++) {
+                            document.getElementById("image"+i).src = "/getIcon?keyIndex="+i+"&time="+(new Date()/1);
+                        }
+                    }
+                    function simulateClick(keyIndex) {
+                        fetch('/click?keyIndex='+keyIndex)
+                        setTimeout(updateImages, 200);
+                        setTimeout(updateImages, 500);
+                    }
+                    updateImages()
+                    setInterval(updateImages, 5000);
+                </script>
+            </body>
+        </html>
+        `)
+    });
+
     expressApp.get('/click', function(req, res, next) {
         const keyIndex = parseInt(req.query.keyIndex,10)
         simulateKeyDown(keyIndex)
